b8c117e78d8f62c69a8280fd000aa12d0c4cffdf
Kristoffer Andersen <ka@ptype.dk>
2016-05-06 09:11:28 +0200

    updated OS X build scripts

--- package/common.sh
+++ package/common.sh
@@ -198,3 +198,42 @@ function copyFiles {
 	echo "Copying $1 to dist..."
 	cp -r $2 $3
 }
+
+function modifyMakefile {
+	ARCH="ARCH=\"../$WIN_GCC_ARM_DIR_NAME/bin/arm-none-eabi-\""
+	echo "replacing GCC file path in makefile to: $ARCH"
+	sed -i.bak "s#ARCH=\".*\"#$ARCH#g" $1/Makefile
+
+	INCLUDE_DIR=../$PSOC5_LIB_NAME/include
+	echo "replacing include dir: $INCLUDE_DIR"
+	sed -i.bak "s#INCLUDE_DIR=.*#INCLUDE_DIR=$INCLUDE_DIR#g" $1/Makefile
+
+	CYPRESS_DIR=../$PSOC5_LIB_NAME/Generated_Source/PSoC5
+	echo "replacing CYPRESS_DIR dir: $CYPRESS_DIR"
+	sed -i.bak "s#CYPRESS_DIR=.*#CYPRESS_DIR=$CYPRESS_DIR#g" $1/Makefile
+
+	CYPRESS_LIB=../$PSOC5_LIB_NAME/lib/monoCyLib.a
+	echo "replacing CYPRESS_LIB dir: $CYPRESS_LIB"
+	sed -i.bak "s#CYPRESS_LIB=.*#CYPRESS_LIB=$CYPRESS_LIB#g" $1/Makefile
+
+	MONO_FRAMEWORK_PATH=../$MONOFRMWRK_NAME
+	echo "replacing MONO_FRAMEWORK_PATH dir: $MONO_FRAMEWORK_PATH"
+	sed -i.bak "s#MONO_FRAMEWORK_PATH=.*#MONO_FRAMEWORK_PATH=$MONO_FRAMEWORK_PATH#g" $1/Makefile
+
+	MBED_FS=../$MBED_LIB_NAME/libraries/fs
+	echo "replacing MBED_FS dir: $MBED_FS"
+	sed -i.bak "s#MBED_FS=.*#MBED_FS=$MBED_FS#g" $1/Makefile
+
+	COMP_LIB=../$PSOC5_LIB_NAME/lib/CyComponentLibrary.a
+	echo "replacing COMP_LIB dir: $COMP_LIB"
+	sed -i.bak "s#COMP_LIB=.*#COMP_LIB=$COMP_LIB#g" $1/Makefile
+
+	MONO_LIB=../$PSOC5_LIB_NAME/lib/monoCyLib.a
+	echo "replacing MONO_LIB dir: $MONO_LIB"
+	sed -i.bak "s#MONO_LIB=.*#MONO_LIB=$MONO_LIB#g" $1/Makefile
+
+	PACKAGE_TARGET=../$DIST_DEST_DIR
+	echo "replacing PACKAGE_TARGET dir: $PACKAGE_TARGET"
+	sed -i.bak "s#PACKAGE_TARGET=.*#PACKAGE_TARGET=$PACKAGE_TARGET#g" $1/Makefile
+
+}
--- package/osx/osx_build.sh
+++ package/osx/osx_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ../configuration.sh
+source ../common.sh
 
 PACKAGE_NAME=OpenMono-v$VERSION-x64.pkg
 BINDIR=../../$BINDIR
@@ -18,81 +19,6 @@ get_abs_filename() {
 	fi
 }
 
-function downloadGcc {
-	FILE=$(basename $1)
-	
-	if [ ! -f $FILE ]; then
-		echo "Downloading GCC Embedded Arm..."
-		curl -O -L $1
-	fi
-	
-	if [ ! -d $GCC_ARM_DIR_NAME ]; then
-		echo "Extracting GCC..."
-		tar -xjf $FILE
-	fi
-	
-	if [ ! -d $GCC_ARM_DIR_NAME ]; then
-		echo "Error: extracted directory is not named $GCC_ARM_DIR_NAME"
-		return 1
-	fi
-	echo "GCC downloaded and extracted"
-}
-
-function copyGcc {
-	echo "Copying GCC to dist dir..."
-    if [ ! -d $2/$1 ]; then
-		mkdir -p "$2"
-		cp -r "$1" "$2"
-	fi
-}
-
-function cloneMonoProg {
-	if [ ! -d $MONOPROG_NAME ]; then
-		echo "Cloing monoprog from GitHub..."
-		git clone $MONOPROG_GIT_URL $MONOPROG_NAME
-	else
-		echo "Pulling monoprog changes from GitHub..."
-		cd $MONOPROG_NAME
-		git pull
-		cd ..
-	fi
-}
-
-function compileMonoprog {
-	echo "Compiling monoprog..."
-	cd $MONOPROG_NAME
-	./compile.sh
-	cd ..
-	echo "Copying to monoprog dist..."
-	mkdir -p $2
-	cp -r $1 $2
-}
-
-function copyFiles {
-	echo "Copying $1 to dist..."
-	cp -r $2 $3
-}
-
-function writeConfigurationFile {
-	echo "Writing monomake configuration file..."
-	echo "#!/bin/bash" > $1
-	DATE=`date`
-	echo "# This is the configuration file for monomake, auto generated $DATE" >> $1
-	echo "" >> $1
-	echo "TEMPLATE_DIR=$(basename $TEMPLATE_DIR)" >> $1
-	echo "MAKEFILE_DIR=\$MONO_PATH" >> $1
-	echo "MONOPROG_DIR=\$MONO_PATH/$MONOPROG_NAME/$(basename $MONOPROG_MAC_EXECUTABLE)/$(dirname $MONOPROG_MAC_EXECUTABLE_BINARY)" >> $1
-}
-
-function makeConfigurationFile {
-	echo "Writing $1..."
-	echo "ARCH=\"\$(MONO_PATH)/$GCC_ARM_DIR_NAME/bin/arm-none-eabi-\"" > $1
-	echo "INCLUDE_DIR=\$(MONO_PATH)/mono/include/mbed/target_cypress" >> $1
-	echo "BUILD_DIR=build" >> $1
-	echo "MONO_FRAMEWORK_PATH=\$(MONO_PATH)/mono" >> $1
-	echo "MONOPROG=\$(MONO_PATH)/monoprog/$2" >> $1
-}
-
 function symbolicLink {
     mkdir -p "$DISTDIR/usr/local/bin"
     ln -s "../openmono/$1" "$DISTDIR/usr/local/bin/$2"
@@ -101,38 +27,46 @@ function symbolicLink {
 # if [ -e "$DISTDIR" ]; then
 # 	rm -rf "$DISTDIR"
 # fi
-
+checkExists git
+clonePsoc5Library
+modifyMakefile $PSOC5_LIB_NAME
+cloneMbedLibrary
+cloneMbedCompLibrary
+modifyMakefile $MBEDCOMP_LIB_NAME
+cloneMonoFramework
+modifyMakefile $MONOFRMWRK_NAME
+buildMonoFramework
 cloneMonoProg
 compileMonoprog $MONOPROG_NAME/$MONOPROG_MAC_EXECUTABLE $DIST_DEST_DIR/monoprog/.
 downloadGcc $GCC_ARM_MAC_URL
-copyGcc $GCC_ARM_DIR_NAME $DIST_DEST_DIR
-copyFiles "binaries" $BINDIR $DIST_DEST_DIR
-copyFiles "framework" $FRAMEWORK_DIR $DIST_DEST_DIR
-copyFiles "templates" $TEMPLATE_DIR $DIST_DEST_DIR
-cp $MAKEFILES $DIST_DEST_DIR
-writeConfigurationFile $DIST_DEST_DIR/configuration.sh
-makeConfigurationFile $DIST_DEST_DIR/Configuration.mk $(basename $MONOPROG_MAC_EXECUTABLE)/$(dirname $MONOPROG_MAC_EXECUTABLE_BINARY)
-symbolicLink bin/monomake monomake
-echo "Package setup done"
-
-echo "Adding Qt libraries to monoprog application bundle..."
-if [ ! hash macdeployqt 2>/dev/null; then
-	echo macdeployqt does not exist, cannot create package.
-	exit 2
-fi
-
-macdeployqt $MONOPROG_NAME/$MONOPROG_MAC_EXECUTABLE -no-plugins
-
-
-if ! hash pkgbuild 2>/dev/null; then
-	echo pkgbuild does not exist, cannot create package.
-	exit 3
-fi
-echo "Building package..."
-pkgbuild \
-	--root "$DISTDIR" \
-	--component-plist component.plist \
-	--identifier com.openmono.monoframework \
-	--version 1 \
-	--install-location / \
-	"$PACKAGE_NAME"
+# copyGcc $GCC_ARM_DIR_NAME $DIST_DEST_DIR
+# copyFiles "binaries" $BINDIR $DIST_DEST_DIR
+# copyFiles "framework" $FRAMEWORK_DIR $DIST_DEST_DIR
+# copyFiles "templates" $TEMPLATE_DIR $DIST_DEST_DIR
+# cp $MAKEFILES $DIST_DEST_DIR
+# writeConfigurationFile $DIST_DEST_DIR/configuration.sh
+# makeConfigurationFile $DIST_DEST_DIR/Configuration.mk $(basename $MONOPROG_MAC_EXECUTABLE)/$(dirname $MONOPROG_MAC_EXECUTABLE_BINARY)
+# symbolicLink bin/monomake monomake
+# echo "Package setup done"
+#
+# echo "Adding Qt libraries to monoprog application bundle..."
+# if [ ! hash macdeployqt 2>/dev/null; then
+#     echo macdeployqt does not exist, cannot create package.
+#     exit 2
+# fi
+#
+# macdeployqt $MONOPROG_NAME/$MONOPROG_MAC_EXECUTABLE -no-plugins
+#
+#
+# if ! hash pkgbuild 2>/dev/null; then
+#     echo pkgbuild does not exist, cannot create package.
+#     exit 3
+# fi
+# echo "Building package..."
+# pkgbuild \
+#     --root "$DISTDIR" \
+#     --component-plist component.plist \
+#     --identifier com.openmono.monoframework \
+#     --version 1 \
+#     --install-location / \
+#     "$PACKAGE_NAME"
