5fdd869c6d5a9c5c8ad89960a209092095b99573
Kristoffer Andersen <ka@ptype.dk>
2016-07-15 14:01:49 +0200

    new monomake version

--- package/win/build-nsis.bat
+++ package/win/build-nsis.bat
@@ -1,6 +1,6 @@
-set VERSION=1.1.2
+set VERSION=1.1.4
 set QTRUNTIME=C:\Qt\Qt5.4.1\5.4\msvc2013_64_opengl
-set ARCH=64
-set VCREDIST=vcredist_x64.exe
+set ARCH=
+set VCREDIST=vcredist_x86.exe
 
 "C:\Program Files (x86)\NSIS\makensis.exe" /V4 setup.nsi
