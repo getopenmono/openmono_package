93472c1e127e971509dfe4a0848c1317163fb2e8
Kristoffer Andersen <ka@ptype.dk>
2016-11-15 16:54:35 +0100

    Updates for 1.4 and macOS builds

--- package/macos/osx_build.sh
+++ package/macos/osx_build.sh
@@ -3,7 +3,7 @@
 source ../configuration.sh
 source ../common.sh
 
-PACKAGE_NAME=OpenMono-v$VERSION-x64.pkg
+PACKAGE_NAME=OpenMono-v$VERSION-Mac
 BINDIR=../../$BINDIR
 FRAMEWORK_DIR=../../$FRAMEWORK_DIR
 TEMPLATE_DIR=../../$TEMPLATE_DIR
@@ -28,24 +28,21 @@ function symbolicLink {
 #     rm -rf "$DISTDIR"
 # fi
 # checkExists git
-# clonePsoc5Library
-# modifyMakefile $PSOC5_LIB_NAME
-# buildPsoc5Library
-# cloneMbedLibrary
-# cloneMbedCompLibrary
-# modifyMakefile $MBEDCOMP_LIB_NAME
-# buildMbedCompLibrary
-# cloneMonoFramework
-# modifyMakefile $MONOFRMWRK_NAME
-# buildMonoFramework
-# cloneMonoProg
-# compileMonoprog "$MONOPROG_NAME/$MONOPROG_MAC_EXECUTABLE" $DIST_DEST_DIR/monoprog/.
-# downloadGcc $GCC_ARM_MAC_URL
-# thinGcc $GCC_ARM_DIR_NAME
-# copyGcc $GCC_ARM_DIR_NAME $DIST_DEST_DIR
-# copyFiles "binaries" $BINDIR $DIST_DEST_DIR
-# copyFiles "templates" $TEMPLATE_DIR $DIST_DEST_DIR
-# cp $MAKEFILES $DIST_DEST_DIR
+cloneMonoFramework
+modifyMakefile $MONOFRMWRK_NAME
+buildMonoFramework
+
+cloneMonoProg
+compileMonoprog "$MONOPROG_NAME/$MONOPROG_MAC_EXECUTABLE" $DIST_DEST_DIR/monoprog/.
+
+# # downloadGcc $GCC_ARM_MAC_URL
+# # thinGcc $GCC_ARM_DIR_NAME
+copyGcc $GCC_ARM_DIR_NAME $DIST_DEST_DIR
+
+copyFiles "binaries" $BINDIR $DIST_DEST_DIR
+copyFiles "templates" $TEMPLATE_DIR $DIST_DEST_DIR
+cp $MAKEFILES $DIST_DEST_DIR
+
 writeConfigurationFile $DIST_DEST_DIR/configuration.sh $(dirname "$(basename $MONOPROG_MAC_EXECUTABLE)/$MONOPROG_MAC_EXECUTABLE_BINARY")
 makeConfigurationFile $DIST_DEST_DIR/predefines.mk $(basename $MONOPROG_MAC_EXECUTABLE)/$MONOPROG_MAC_EXECUTABLE_BINARY
 symbolicLink bin/monomake monomake
@@ -56,9 +53,7 @@ if [ ! hash macdeployqt 2>/dev/null; then
     echo macdeployqt does not exist, cannot create package.
     exit 2
 fi
-
-macdeployqt $MONOPROG_NAME/$MONOPROG_MAC_EXECUTABLE -no-plugins
-
+macdeployqt $DIST_DEST_DIR/monoprog/monoprog.app -no-plugins
 
 if ! hash pkgbuild 2>/dev/null; then
     echo pkgbuild does not exist, cannot create package.
@@ -73,4 +68,8 @@ pkgbuild \
     --version $VERSION \
     --install-location / \
     --scripts scripts \
-    "$PACKAGE_NAME"
+    "$PACKAGE_NAME.pkg"
+
+echo "Creating TGZ Mac package for those who hate PKG installers..."
+tar -czf "$PACKAGE_NAME.tgz" -C "`dirname $DIST_DEST_DIR`" "`basename $DIST_DEST_DIR`"
+echo "All Done"
