31cba40c61b5b8145dc595906f3cebca17b34ef7
Kristoffer Andersen <ka@openmono.com>
2016-11-22 10:26:42 +0100

    incrment version

--- package/debian/install.sh
+++ package/debian/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.4.2
+VERSION=1.4.3
 GIT_TAG="SDKv1_4"
 DOWNLOAD_URL="https://github.com/getopenmono/openmono_package/releases/download/$GIT_TAG/openmono_$VERSION.deb"
 FILENAME=`basename $DOWNLOAD_URL`
