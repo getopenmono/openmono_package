c07bfd7ba8f81f2a1b41495c2320b2ebf04e5198
Kristoffer Andersen <ka@ptype.dk>
2017-05-26 11:32:54 +0200

    remembered to change the vcdist filename

--- package/win/build-nsis.bat
+++ package/win/build-nsis.bat
@@ -1,6 +1,6 @@
 set VERSION=1.4.3
 set QTRUNTIME=C:\Qt\Qt5.4.1\5.4\msvc2013_64_opengl
 set ARCH=
-set VCREDIST=vc_redist.x86.exe
+set VCREDIST=vcredist_x86.exe
 
 "C:\Program Files (x86)\NSIS\makensis.exe" /V4 setup.nsi
