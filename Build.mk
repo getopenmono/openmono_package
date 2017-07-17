393ab469ac1f36024ef793f98cdb53bf74fa81d7
Kristoffer Andersen <stoffera@gmail.com>
2017-07-18 00:03:23 +0200

    project makefiles are cross platform (better windows support)

--- Build.mk
+++ Build.mk
@@ -21,7 +21,7 @@ MBED_INCLUDES = $(MONO_FRAMEWORK_PATH)/include/mbed \
 				$(MONO_FRAMEWORK_PATH)/include/mbed/target_cypress \
 				$(MONO_FRAMEWORK_PATH)/include/mbed/libraries/fs/sd \
 				$(MONO_FRAMEWORK_PATH)/include/mbed/libraries/fs/fat \
-				$(MONO_FRAMEWORK_PATH)/include/mbed/libraries/fs/fat/ChaN 
+				$(MONO_FRAMEWORK_PATH)/include/mbed/libraries/fs/fat/ChaN
 
 MONO_INCLUDES = $(MONO_FRAMEWORK_PATH)/include \
 				$(MONO_FRAMEWORK_PATH)/include/display \
@@ -51,10 +51,10 @@ CDEFS=
 ASDEFS=
 AS_FLAGS = -c -g -Wall -mcpu=cortex-m3 -mthumb -mthumb-interwork -march=armv7-m
 CC_FLAGS = -c -g -Wall -mcpu=cortex-m3 -mthumb $(OPTIMIZATION) -mthumb-interwork -fno-common -fmessage-length=0 -ffunction-sections -fdata-sections -march=armv7-m
-ONLY_C_FLAGS = -std=gnu99 
+ONLY_C_FLAGS = -std=gnu99
 ONLY_CPP_FLAGS = -std=gnu++98 -fno-rtti -fno-exceptions
 LDSCRIPT = -T $(LINKER_SCRIPT)
-LD_FLAGS = -g -mcpu=cortex-m3 -mthumb -march=armv7-m -fno-rtti -Wl,--gc-sections -specs=nano.specs 
+LD_FLAGS = -g -mcpu=cortex-m3 -mthumb -march=armv7-m -fno-rtti -Wl,--gc-sections -specs=nano.specs
 LD_SYS_LIBS = -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys
 
 COPY_FLAGS = -j .text -j .eh_frame -j .rodata -j .ramvectors -j .noinit -j .data -j .bss -j .stack -j .heap -j .cyloadablemeta
@@ -62,26 +62,26 @@ COPY_FLAGS = -j .text -j .eh_frame -j .rodata -j .ramvectors -j .noinit -j .data
 all: clean $(BUILD_DIR) $(TARGET).elf
 
 $(BUILD_DIR):
-	@echo "creating build directory"
+	@echo "creating build directory";
 	@mkdir -p ./$(BUILD_DIR)
 
 $(BUILD_DIR)/mono_default_main.o: $(MONO_PATH)/mono_default_main.cpp
-	@echo "Compiling C++: Default main function"
+	@echo "Compiling C++: Default main function";
 	@$(MKDIR) -p $(dir $@)
 	@$(CXX) $(CC_FLAGS) $(ONLY_CPP_FLAGS) $(CDEFS) $(INCS) -o $@ $<
 
 $(BUILD_DIR)/%.o: %.c $(TARGET_HEADERS)
-	@echo "Compiling C: $<"
+	@echo "Compiling C: $<";
 	@$(MKDIR) -p $(dir $@)
 	@$(CC) $(CC_FLAGS) $(ONLY_C_FLAGS) $(CDEFS) $(INCS) -o $@ $<
 
 $(BUILD_DIR)/%.o: %.cpp $(TARGET_HEADERS)
-	@echo "Compiling C++: $<"
+	@echo "Compiling C++: $<";
 	@$(MKDIR) -p $(dir $@)
 	@$(CXX) $(CC_FLAGS) $(ONLY_CPP_FLAGS) $(CDEFS) $(INCS) -o $@ $<
 
 $(TARGET).elf: $(MONO_FRAMEWORK_PATH)/monoCyLib.a $(MONO_FRAMEWORK_PATH)/CyComponentLibrary.a $(MONO_FRAMEWORK_PATH)/mbedlib.a $(MONO_FRAMEWORK_PATH)/mono_framework.a $(TARGET_OBJECTS) $(BUILD_DIR)/mono_default_main.o
-	@echo "Linking $(notdir $@)"
+	@echo "Linking $(notdir $@)";
 	@$(LD) -Wl,--start-group $(LD_FLAGS) -o $@ $^ -mthumb -march=armv7-m -mfix-cortex-m3-ldrd "-Wl,-Map,mono_project.map" -T $(LINKER_SCRIPT) -g  "-u\ _printf_float" $(LD_SYS_LIBS) -Wl,--gc-sections -Wl,--end-group
 
 $(TARGET).hex: $(TARGET).elf
@@ -90,27 +90,27 @@ $(TARGET).hex: $(TARGET).elf
 	$(ELFTOOL) -B $^ --flash_size $(FLASH_SIZE) --flash_array_size $(FLASH_ARRAY_SIZE) --flash_row_size $(FLASH_ROW_SIZE) --ee_array $(EE_ARRAY) --ee_row_size $(EE_ROW_SIZE)
 
 $(TARGET):  $(OBJS)  ${LINKER_SCRIPT}
-	@echo "Other link: $(OBJS)"
+	@echo "Other link: $(OBJS)";
 	@$(LD) $(LDSCRIPT) $(OBJS) -o $@
 
 
 systemFiles:
-	@echo $(SYS_OBJECTS)
-	
+	@echo $(SYS_OBJECTS);
+
 appFiles:
-	@echo $(OBJECTS)
+	@echo $(OBJECTS);
 
 mbedFiles:
-	@echo $(MBED_INCLUDES)
+	@echo $(MBED_INCLUDES);
 
 monoFiles:
-	@echo $(MONO_INCLUDES)
+	@echo $(MONO_INCLUDES);
 
-includeFiles: 
-	@echo $(INCS)
+includeFiles:
+	@echo $(INCS);
 
 install: all
-	@echo "Programming app to device..."
+	@echo "Programming app to device...";
 	$(MONOPROG) -p $(TARGET).elf --verbose 1
 
 clean:
@@ -119,4 +119,4 @@ clean:
 
 summary: $(TARGET).elf
 	$(ELFTOOL) -S $(TARGET).elf
-	
+
--- mono.mk
+++ mono.mk
@@ -1,9 +1,9 @@
 include $(MONO_PATH)/Build.mk
 
 paths:
-	@echo "GCC: $(ARCH)"
-	@echo "Monoprog: $(MONOPROG)"
-	@echo "Mono Framework: $(MONO_FRAMEWORK_PATH)"
-	@echo "Build dir: $(BUILD_DIR)"
-	@echo "Include dir: $(INCLUDE_DIR)"
-	@echo $(TARGET_OBJECTS)
+	@echo "GCC: $(ARCH)";
+	@echo "Monoprog: $(MONOPROG)";
+	@echo "Mono Framework: $(MONO_FRAMEWORK_PATH)";
+	@echo "Build dir: $(BUILD_DIR)";
+	@echo "Include dir: $(INCLUDE_DIR)";
+	@echo "$(TARGET_OBJECTS)";
